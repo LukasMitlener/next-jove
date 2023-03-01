f73b757b21ca26f65a0577607518988673d36675
LukasMitlener <97617930+LukasMitlener@users.noreply.github.com>
2023-03-01 20:48:24 +0100

    Add files via upload

--- components/Pricing.jsx
+++ components/Pricing.jsx
@@ -1,11 +1,10 @@
-import styles from "../styles/styles";
 import { CheckIcon, XIcon } from '@heroicons/react/solid';
 
 
 const Pricing = () => {
   return (
     <div name='cena' className='w-full mt-24'>
-      <div className='max-w-[1240px] mx-auto px-12'>
+      <div className='max-w-[1240px] mx-auto px-6'>
 
         <div className='px-4 py-12'>
           <h2>
@@ -19,12 +18,12 @@ const Pricing = () => {
           </p> */}
         </div>
 
-        <div className='grid md:grid-cols-2'>
+        <div className='grid md:grid-cols-2 gap-8'>
 
-          <div className='bg-white text-slate-900 m-4 p-8 rounded-xl shadow-xl relative'>
+          <div className='bg-white text-slate-900 p-8 rounded-xl shadow-xl relative'>
             <span className='uppercase px-3 py-1 bg-main-blue text-white rounded-2xl text-LG'>Standard</span>
             <div>
-              <p className='text-5xl font-bold py-4 flex'>8 999 Kč</p>
+              <p className='text-5xl font-bold py-4 flex text-black'>8 999 Kč</p>
             </div>
             <h3>Základní model lyží.</h3>
             <div className='text-lg'>
@@ -32,14 +31,14 @@ const Pricing = () => {
                 <p className='flex py-4'><CheckIcon className='w-8 mr-5 text-main-blue'  />...</p>
                 <p className='flex py-4'><CheckIcon className='w-8 mr-5 text-main-blue'  />...</p>
                 <p className='flex py-4'><CheckIcon className='w-8 mr-5 text-main-blue'  />Montáž vázání.</p>
-                <p className='flex py-4'><XIcon className='w-8 mr-5 text-main-blue'  />Vylepšená kolečka.</p>
+                <p className='flex py-4'><XIcon className='w-8 mr-5 text-red-400'  />Vylepšená kolečka.</p>
             </div>
           </div>
 
-          <div className='bg-white text-slate-900 m-4 p-8 rounded-xl shadow-xl relative'>
+          <div className='bg-white text-slate-900 p-8 rounded-xl shadow-xl relative'>
             <span className='uppercase px-3 py-1 bg-main-blue text-white rounded-2xl text-LG'>Premium</span>
             <div>
-              <p className='text-5xl font-bold py-4 flex'>11 999 Kč</p>
+              <p className='text-5xl font-bold py-4 flex text-black'>11 999 Kč</p>
             </div>
             <h3>Nejnovější zdokonalený model.</h3>
             <div className='text-lg'>
