c85af5ffc57e4c129c7272fe9cf29fbea7711964
LukasMitlener <97617930+LukasMitlener@users.noreply.github.com>
2023-03-16 21:18:00 +0100

    Add files via upload

--- components/Proc.jsx
+++ components/Proc.jsx
@@ -11,8 +11,8 @@ const Proc = () => {
             </h2>
           </div>
 
-          <div className='grid grid-cols-1 lg:grid-cols-3 relative gap-x-8 gap-y-16 px-12 sm:pt-20 pt-12'>
-              <div className='bg-white rounded-lg shadow-2xl flex flex-col justify-between'>
+          <div className='grid grid-cols-1 lg:grid-cols-3 relative gap-x-8 gap-y-16 sm:pt-20 pt-12'>
+              <div className='bg-white rounded-lg shadow-xl flex flex-col justify-between'>
                   <div className='p-8'>
                       <HeartIcon className='w-16 p-4 bg-main-blue text-white rounded-lg mt-[-4rem]' />
                       <h3>Český výrobek</h3>
@@ -22,7 +22,7 @@ const Proc = () => {
                     <Link to="cta" smooth={true}><p className='flex items-center text-main-blue text-xl text-center font-semibold'>Objednat</p></Link>
                   </div>
               </div>
-              <div className='bg-white rounded-lg shadow-2xl flex flex-col justify-between'>
+              <div className='bg-white rounded-lg shadow-xl flex flex-col justify-between'>
                   <div className='p-8'>
                       <FingerPrintIcon className='w-16 p-4 bg-main-blue text-white rounded-lg mt-[-4rem]' />
                       <h3>Unikátní vlastnosti</h3>
@@ -32,7 +32,7 @@ const Proc = () => {
                     <Link to="cta" smooth={true}><p className='flex items-center text-main-blue text-xl font-semibold'>Objednat</p></Link>
                   </div>
               </div>
-              <div className='bg-white rounded-lg shadow-2xl flex flex-col justify-between'>
+              <div className='bg-white rounded-lg shadow-xl flex flex-col justify-between'>
                   <div className='p-8'>
                       <CashIcon className='w-16 p-4 bg-main-blue text-white rounded-lg mt-[-4rem]' />
                       <h3>Poměr cena výkon</h3>
