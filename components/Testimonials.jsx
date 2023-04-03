7b5e2617504c04ba5f5ba2aed4617d36236d3c6f
LukasMitlener <97617930+LukasMitlener@users.noreply.github.com>
2023-04-03 11:47:43 +0200

    Add files via upload

--- components/Testimonials.jsx
+++ components/Testimonials.jsx
@@ -3,29 +3,24 @@ const Testimonials = () => {
   return (
     <div name='testimonials' className="w-full mt-24">
       <div className='max-w-[1240px] mx-auto px-6'>  
-        <div className='px-4 py-12'>
-          <h2>
-          ZKUŠENOSTI ZÁKAZNÍKŮ S LYŽEMI <span className="text-main-blue">JOVE</span>
-          </h2>
-        </div>
+
         <div className="mt-12 grid grid-cols-1 gap-8 sm:gap-16 lg:grid-cols-3">
           <div className="bg-white">
-            <img
-              alt="Woman"
-              src="https://images.unsplash.com/photo-1599566219227-2efe0c9b7f5f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
-              className="mx-auto h-24 w-24 rounded-full object-cover shadow-xl"
-            />
-
             <blockquote
               className="-mt-6 flex flex-col justify-between rounded-lg p-12 text-center shadow-xl"
             >
-              <p className="text-lg font-bold">Standa Řezáč</p>
-              <p className="mt-1 text-xs font-medium text-gray-500">
-              dlouholetý  reprezentant v dálkových bězích a trojnásobný vítěz Jizerské 50 a naposledy  3.  na MČR 2023 v běhu na  lyžích na 50 km
+              <p className="text-2xl font-bold">Standa Řezáč</p>
+              <p className="mt-1 text-sm font-medium text-gray-500">
+              dlouholetý reprezentant v dálkových bězích a trojnásobný vítěz Jizerské 50
               </p>
+              <div>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="inline-block w-8 h-8 text-main-blue mt-8" viewBox="0 0 975.036 975.036">
+                  <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
+                </svg>
+              </div>
               <p className="mt-4">
-              " Kolečkové lyže  JOVE jsem  vyhrál  před několika lety na závodech  na  kol. lyžích , který  firma JOVE pořádala   ze  Špindlu na Špindlerovku .
-Jsou výborné na  vyjížďky  po všech cestách a díky brzdám  po Jizerkách  se na nich  dá sjet  téměř  vše.  Oblíbil si je můj mladej . který mi je zabavil a trénuje na nich . Doporučuji . "
+              Kolečkové lyže JOVE jsem vyhrál před několika lety na závodech na kol. lyžích ze  Špindlu na Špindlerovku, které firma JOVE pořádala.
+              Jsou výborné na vyjížďky po všech cestách po Jizerkách a díky brzdám se na nich dá sjet téměř vše. Oblíbil si je můj mladej, který mi je zabavil a trénuje na nich. Doporučuji.
               </p>
 
               <div className="mt-8 flex justify-center gap-0.5 text-main-blue">
@@ -84,21 +79,20 @@ Jsou výborné na  vyjížďky  po všech cestách a díky brzdám  po Jizerkác
           </div>
 
           <div className="bg-white">
-            <img
-              alt="Woman"
-              src="https://images.unsplash.com/photo-1599566219227-2efe0c9b7f5f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
-              className="mx-auto h-24 w-24 rounded-full object-cover shadow-xl"
-            />
-
             <blockquote
               className="-mt-6 flex flex-col justify-between rounded-lg p-12 text-center shadow-xl"
             >
-              <p className="text-lg font-bold text-gray-700">Blanka Paulů</p>
-              <p className="mt-1 text-xs font-medium text-gray-500">
-              bývalá reprezentantka  a nositelka  stříbrné medaile  z MS  v běhu na 10 km a olympijská medailistka ze štafety z ZOH v Sarajevu
+              <p className="text-2xl font-bold text-gray-700">Blanka Paulů</p>
+              <p className="mt-1 text-sm font-medium text-gray-500">
+              bývalá reprezentantka a nositelka stříbrné medaile z MS v běhu na 10 km a olympijská medailistka ze štafety z ZOH v Sarajevu
               </p>
+              <div>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="inline-block w-8 h-8 text-main-blue mt-8" viewBox="0 0 975.036 975.036">
+                  <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
+                </svg>
+              </div>
               <p className="mt-4">
-              "  Lyže JOVE jsou  super díky nafukovacím kolečkům  pro jízdu zejména  na našich rozbitých cestách. Vyhovovalo mně ,že jsem  si mohla   z kopce přibrzďovat  a sjet tak i  úseky  na které bych si na jiných kolečkových lyžích jinak netroufla."
+              Lyže JOVE jsou super díky nafukovacím kolečkům pro jízdu zejména na našich rozbitých cestách. Vyhovovalo mně, že jsem si mohla z kopce přibrzďovat a sjet tak i úseky, na které bych si na jiných kolečkových lyžích jinak netroufla.
               </p>
 
               <div className="mt-8 flex justify-center gap-0.5 text-main-blue">
@@ -157,21 +151,20 @@ Jsou výborné na  vyjížďky  po všech cestách a díky brzdám  po Jizerkác
           </div>
 
           <div className="bg-white">
-            <img
-              alt="Woman"
-              src="https://images.unsplash.com/photo-1599566219227-2efe0c9b7f5f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
-              className="mx-auto h-24 w-24 rounded-full object-cover shadow-xl"
-            />
-
             <blockquote
               className="-mt-6 flex flex-col justify-between rounded-lg p-12 text-center shadow-xl"
             >
-              <p className="text-lg font-bold text-gray-700">Zděnek Vejnar</p>
-              <p className="mt-1 text-xs font-medium text-gray-500">
-              Bývalý trenér  rakouské reprezentace v biatlonu
+              <p className="text-2xl font-bold text-gray-700">Zděnek Vejnar</p>
+              <p className="mt-1 text-sm font-medium text-gray-500">
+              bývalý trenér rakouské reprezentace v biatlonu
               </p>
+              <div>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="inline-block w-8 h-8 text-main-blue mt-8" viewBox="0 0 975.036 975.036">
+                  <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
+                </svg>
+              </div>
               <p className="mt-4">
-              " Na JOVE lyžích jsem měl možnost jezdit a doporučuji  je mladým závodníkům , kteří potřebují  dostat správnou techniku bruslení do krve,  protože  jim neodpustí  žádnou  chybu při  přenášení váhy  a tím  mohou upevňovat správné  technické návyky. ..." 
+              Na JOVE lyžích jsem měl možnost jezdit a doporučuji je mladým závodníkům, kteří potřebují dostat správnou techniku bruslení do krve, protože jim neodpustí žádnou chybu při přenášení váhy a tím mohou upevňovat správné technické návyky. 
               </p>
 
               <div className="mt-8 flex justify-center gap-0.5 text-main-blue">
