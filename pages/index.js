2651193ee875cbe369e02ef9c203f278bce249bb
LukasMitlener <97617930+LukasMitlener@users.noreply.github.com>
2023-07-04 15:59:47 +0200

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,10 @@ import About from "../components/About"
 import Proc from "../components/Proc"
 import Video from "../components/Video"
 import Blog from "../components/Blog"
-import Pricing from "../components/Pricing"
 import Testimonials from "../components/Testimonials"
 import Cta from "../components/Cta"
 import Footer from "../components/Footer"
+import Pricing from "../components/Pricing"
 
 export default function Home() {
   return (
@@ -16,14 +16,14 @@ export default function Home() {
       <Head>
         <title>Kolečkové lyže Jove</title>
         <meta name="description" content="Unikátní kolečkové lyže Jove nabízejí skvělé jízdní vlastnosti pro letní trénink. Mnoho barevných možností. Navrženo a vyrobeno zkušeným sportovcem." />
-
         <meta name="keywords" content="kolečkové lyže, Jove, letní trénink, letní běžky, běžky na kolečkách, běžky v létě, na suchu, klasika, skate, na soupaž, barevné provedení, unikátní, originální, jak vybrat" />
-
+        
         <meta property="og:title" content="Kolečkové lyže Jove - Unikátní lyže pro letní trénink" />
         <meta property="og:description" content="Unikátní kolečkové lyže Jove nabízejí skvělé jízdní vlastnosti pro letní trénink. Mnoho barevných možností. Navrženo a vyrobeno zkušeným sportovcem." />
-        <meta property="og:image" content="https://example.com/images/cool-page.jpg" />
+        <meta property="og:image" content="https://jove-rollerski.com/joveOG.jpg" />
         <meta property="og:url" content="https://jove-rollerski.com" />
         <meta property="og:type" content="website" />
+
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
